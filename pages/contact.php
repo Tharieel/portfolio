ac6d2cfcde826bf565c128921adffc8fc3ea6282
Tharieel <thari.business@protonmail.com>
2026-04-10 19:37:20 +0200

    projekt-page

---
+++ pages/contact.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Empfänger (DEINE Mail!)
+$to = "deine-email@example.com";
+
+// Betreff
+$subject = "Neue Kontaktanfrage";
+
+// Nachricht
+$body = "Name: $name\nE-Mail: $email\n\nNachricht:\n$message";
+
+// Header
+$headers = "From: $email";
+
+// Mail senden
+mail($to, $subject, $body, $headers);
+
+// Antwort
+echo "Danke $name, deine Nachricht wurde gesendet!";
+?>
